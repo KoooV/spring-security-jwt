edf66ad9ec01f593c8505fd000b8e727b15dbe5e
KoV <alevkovalev13681@gmail.com>
2025-08-13 23:06:13 +0300

    add UserService.java

--- src/main/java/com/kov/springsecurityjwt/service/UserService.java
+++ src/main/java/com/kov/springsecurityjwt/service/UserService.java
@@ -1,7 +1,29 @@
 package com.kov.springsecurityjwt.service;
 
+import com.kov.springsecurityjwt.model.User;
+import com.kov.springsecurityjwt.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+
+@RequiredArgsConstructor
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+
+    public User registerUser(String name, String password) {
+        String hashedPassword = passwordEncoder.encode(password);
+        User user = new User();
+        user.setName(name);
+        user.setPasswordHash(hashedPassword);
+        return userRepository.save(user);
+    }
+    public User findByName(String name){
+        return userRepository.findByName(name);
+
+    }
+
 }
