37c95c9e305959ad98e4f997b330d85238647459
KoV <alevkovalev13681@gmail.com>
2025-08-14 19:05:35 +0300

    add UserController.java & RegisterRequestDto.java

---
+++ src/main/java/com/kov/springsecurityjwt/Dto/RegisterRequestDto.java
@@ -0,0 +1,15 @@
+package com.kov.springsecurityjwt.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class RegisterRequestDto {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/kov/springsecurityjwt/UserController/UserController.java
@@ -0,0 +1,21 @@
+package com.kov.springsecurityjwt.UserController;
+
+import com.kov.springsecurityjwt.Dto.RegisterRequestDto;
+import com.kov.springsecurityjwt.model.User;
+import com.kov.springsecurityjwt.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/users/")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    public User register(@RequestBody RegisterRequestDto registerRequestDto) {
+        return userService.registerUser(registerRequestDto.getUsername(), registerRequestDto.getPassword());
+    }
+
+}
