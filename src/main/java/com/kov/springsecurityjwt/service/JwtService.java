1b1ee98fc35733bab7af28ff4a37563b2dd756cb
KoV <alevkovalev13681@gmail.com>
2025-08-16 16:51:46 +0300

    update JwtService.java

--- src/main/java/com/kov/springsecurityjwt/service/JwtService.java
+++ src/main/java/com/kov/springsecurityjwt/service/JwtService.java
@@ -1,9 +1,12 @@
 package com.kov.springsecurityjwt.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -12,7 +15,10 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
+
+@Slf4j
 @Service
 public class JwtService {
 
@@ -22,8 +28,9 @@ public class JwtService {
     @Value("${jwt.expiration}")
     private long expiration;
 
+
     public String generateToken(UserDetails userDetails) {
-        return generateToken(new HashMap<>(), userDetails );
+        return generateToken(new HashMap<>(), userDetails);
     }
 
     public String generateToken(Map<String, Object> claims, UserDetails userDetails) {
@@ -36,9 +43,56 @@ public class JwtService {
                 .compact();
     }
 
-    private Key getSignInKey() {
+    public Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(secret);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
-}
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token) && validateSignature(token));
+    }
+
+    public boolean validateSignature(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(getSignInKey())
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SignatureException e) {
+            log.error("Invalid JWT signature: {}", e.getMessage());
+            return false;
+
+        }
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());//возвращает true если дата токена раньше текущей даты
+    }
+
+    private Date extractExpiration(String token) {//вытаскиваем из payload время актуальности токена
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+    private String extractUsername (String token){
+        return extractClaim(token, Claims::getSubject);//вытаскиеваем из payload имя
+        }
+
+
+
+    }
+
